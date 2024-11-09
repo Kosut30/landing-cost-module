b438d1bac4bc7bcc08adbcaf30591f12266c5844
Kosut30 <kipchumbachege@gmail.com>
2024-11-09 20:04:08 +1000

    integrate backend API responses to frotend react native

--- frontend/components/ImportsManagement.js
+++ frontend/components/ImportsManagement.js
@@ -32,7 +32,7 @@ const ImportsManagement = () => {
 
     try {
       // Make a POST request to the backend to add the new import
-      await axios.post('http://192.168.100.3:5000/api/imports/add', newImport);
+      await axios.post('http://192.168.46.155:5000/api/imports/add', newImport);
       Alert.alert('Success', 'Import added successfully!');
 
       // Clear form inputs after submission
